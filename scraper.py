34276c4a2a90f8b62cf9c51f02057159a1a4562b
almusfuad <almusfuad@gmail.com>
2024-10-26 15:15:49 +0600

    [add] code for spinner and solve error on production

--- scraper.py
+++ scraper.py
@@ -16,27 +16,31 @@ with st.form(key='Search'):
       search = st.form_submit_button('Search')
 placeholder = st.empty()      # placeholder helps to display different images in the same row
 if keyword:
-      page=requests.get(f'https://unsplash.com/s/photos/{keyword}')
-      soup = BeautifulSoup(page.content, 'lxml')
-      rows = soup.find_all('div', class_='bugb2')
-      col1, col2 = placeholder.columns(2)
-      for index,row in enumerate(rows):
-            figures = row.find_all('figure')
-            for i in range(2):
-                  img = figures[i].find('img', class_='I7OuT DVW3V L1BOa')
-                  list=img['srcset'].split('?')
-                  anchor = figures[i].find('a', class_='zNNw1')
-                  print(anchor['href'])
-                  if i==0:
-                        with col1:
-                              st.image(list[0])
-                              btn=st.button('Download', key=str(index)+str(i))  # Unique key built
-                              if btn:
-                                    webbrowser.open_new_tab('https://unsplash.com'+anchor['href'])
-                  else:
-                        with col2:
-                              st.image(list[0])
-                              btn = st.button('Download', key=str(index)+str(i))
-                              if btn:
-                                    webbrowser.open_new_tab('https://unsplash.com'+anchor['href'])
+      with st.spinner('Searching for images...'):
+            try:
+                  page=requests.get(f'https://unsplash.com/s/photos/{keyword}')
+                  soup = BeautifulSoup(page.content, 'lxml')
+                  rows = soup.find_all('div', class_='bugb2')
+                  col1, col2 = placeholder.columns(2)
+                  for index,row in enumerate(rows):
+                        figures = row.find_all('figure')
+                        for i in range(2):
+                              img = figures[i].find('img', class_='I7OuT DVW3V L1BOa')
+                              list=img['srcset'].split('?')
+                              anchor = figures[i].find('a', class_='zNNw1')
+                              print(anchor['href'])
+                              if i==0:
+                                    with col1:
+                                          st.image(list[0])
+                                          btn=st.button('Download', key=str(index)+str(i))  # Unique key built
+                                          if btn:
+                                                webbrowser.open_new_tab('https://unsplash.com'+anchor['href'])
+                              else:
+                                    with col2:
+                                          st.image(list[0])
+                                          btn = st.button('Download', key=str(index)+str(i))
+                                          if btn:
+                                                webbrowser.open_new_tab('https://unsplash.com'+anchor['href'])
+            except requests.exceptions.RequestException as e:
+                  st.error(f"Error: {e}")
                   
